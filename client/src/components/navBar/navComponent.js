f03033b8e6e43dd8f5855afae0216ede4ad19c02
Eddyking1 <edward.kumerius@hotmail.com>
2020-05-06 18:48:52 +0200

    Nav changes depending on screen width with conditional rendering, one render = one screen width = hamburger nav or desktop nav, if change screenwidth below requirement to change refresh is needed because there is no reason for nav to be updated more than first initialisation

--- client/src/components/navBar/navComponent.js
+++ client/src/components/navBar/navComponent.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import { Link } from "react-router-dom";
 import './nav.scss';
 
 
-export default function NavBar() {
-    const [scrollY, setScrollY] = useState();
+export default function NavBar () {
     const [showHamburger, setShowHamburger] = useState(false);
-    const [hamCss, setHamCss] = useState(['hamContainerAnimate', 'hamContainer']);
+    const [screenWidth, setScreenWidth] = useState(0); 
 
     function hamChecker() {
         var x = document.getElementsByClassName('hamContainer')[0];
@@ -20,88 +19,82 @@ export default function NavBar() {
         }
     }
 
-    useEffect(() => {
+    function checkWidth () {
+        setScreenWidth(window.innerWidth);
+    }
 
+    useLayoutEffect(() => {
+        checkWidth ()
     });
 
     return (
-        <div className="hamNav">
-            <button className="navButton" onClick={() => { hamChecker() }}>
-                <i className="material-icons">
-                    reorder
-                </i>
-            </button>
+        <div>
+            {screenWidth < 900 ? (
+                <div className="hamNav">
+                    <button className="navButton" onClick={() => { hamChecker() }}>
+                        <i className="material-icons">reorder</i>
+                    </button>
+                    <div className="hamContainer">
+                        <div className="linkContainer">
 
-            <div className="hamContainer">
-                <div className="linkContainer">
+                            <Link to="./" className="hamLinks" onClick={() => { hamChecker() }}>
+                                <p> Home </p>
+                            </Link>
 
-                    <Link to="./" className="hamLinks" onClick={() => { hamChecker() }}>
-                        <p> Home </p>
-                    </Link>
+                            <Link to="/login" className="hamLinks" onClick={() => { hamChecker() }}>
+                                <p> Login </p>
+                            </Link>
 
-                    <Link to="/login" className="hamLinks" onClick={() => { hamChecker() }}>
-                        <p> Login </p>
-                    </Link>
+                            <Link to="/signup" className="hamLinks" onClick={() => { hamChecker() }}>
+                                <p> Sign Up </p>
+                            </Link>
 
-                    <Link to="/signup" className="hamLinks" onClick={() => { hamChecker() }}>
-                        <p> Sign Up </p>
-                    </Link>
+                            <Link to="/shop" className="hamLinks" onClick={() => { hamChecker() }}>
+                                <p> Store </p>
+                            </Link>
+                        </div>
 
-                    <Link to="/shop" className="hamLinks" onClick={() => { hamChecker() }}>
-                        <p> Store </p>
-                    </Link>
-                    
+                        <div className="socialIcons">
+                            <a href="https://github.com/eddyking1">
+                                <i className="fab fa-github-square fa-3x"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                
-                <div className="socialIcons">
-                <a href="https://github.com/eddyking1">
-                <i class="fab fa-github-square fa-3x"></i>
-                </a>
+            )
+                :(<div className="nav">
+                    <div className="navBarContainer">
+                        <button className="navButton">
+                            <Link to="/" className="links">
+                                <i className="material-icons">house</i>
+                                <p> Home </p>
+                            </Link>
+                        </button>
+
+                        <button className="navButton">
+                            <Link to="/login" className="links">
+                                <i className="material-icons">meeting_room</i>
+                                <p> Login </p>
+                            </Link>
+                        </button>
+
+                        <button className="navButton">
+                            <Link to="/signup" className="links">
+                                <i className="material-icons">add_circle</i>
+                                <p> Sign Up </p>
+                            </Link>
+                        </button>
+
+                        <button className="navStoreButton">
+                            <Link to="/shop" className="links">
+                                <i className="material-icons">shopping_cart</i>
+                                <p> Store </p>
+                            </Link>
+                        </button>
+                    </div>
                 </div>
-            </div>
+                )}
         </div>
-
     )
 }
 
-
-
-
-/* <div className="nav">
-            <div className="navBarContainer">
-                <button className="navButton">
-                    <Link to="/" className="links">
-                        <i className="material-icons">
-                            house
-                        </i>
-                        <p> Home </p>
-                    </Link>
-                </button>
-
-                <button className="navButton">
-                    <Link to="/login" className="links">
-                        <i className="material-icons">
-                            meeting_room
-                        </i>
-                        <p> Login </p>
-                    </Link>
-                </button>
-
-                <button className="navButton">
-                    <Link to="/signup" className="links">
-                        <i className="material-icons">
-                            add_circle
-                        </i>
-                        <p> Sign Up </p>
-                    </Link>
-                </button>
-
-                <button className="navStoreButton">
-                    <Link to="/shop" className="links">
-                        <i className="material-icons">shopping_cart
-                        </i>
-                        <p> Store </p>
-                    </Link>
-                </button>
-            </div>
-        </div> */

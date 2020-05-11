4d7ea314706ee6a698544d60cd7601fab8580642
Eddyking1 <edward.kumerius@hotmail.com>
2020-05-11 10:56:58 +0200

    fix

--- config/keys.js
+++ config/keys.js
@@ -4,6 +4,6 @@ if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
   } else {
     // we are in development - return the dev keys!!!
-    module.exports = require('./dev.js');
+    module.exports = require('./dev');
   }
   

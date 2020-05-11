a924299d288cf295bba4ab0a75b130e50ec2c15a
Eddyking1 <edward.kumerius@hotmail.com>
2020-05-11 11:16:49 +0200

    import fix typos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
-const keys = require('./config/dev.js');
+const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
@@ -28,13 +28,13 @@ require('./routes/billingRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
+  app.use(express.static('client/public'));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   });
 }
 

6bd2b31dedc49a983f8d8ac419eb27fafd8248f6
Eddyking1 <edward.kumerius@hotmail.com>
2020-05-16 21:24:49 +0200

    new way of global store

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -23,9 +23,8 @@ export default function Login() {
       try {
         const result = await axios(url);
         if (result.data) {
-         Object.keys(result.data).map((items, index) => (
-           dispatch({ type: FETCH_USER, payload: items })
-           ));
+           dispatch({ type: FETCH_USER, payload: result.data })
+          console.log(result.data);
         }
       }
       catch (error) {

529329c7ce356240cb017da8e18bc3620e7a8a12
Eddyking1 <edward.kumerius@hotmail.com>
2020-05-18 17:26:42 +0200

    backend mistake fix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,4 @@
 const passport = require('passport');
-const User = mongoose.model('users');
-
 module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google', 
     { scope: ['profile', 'email'] }));

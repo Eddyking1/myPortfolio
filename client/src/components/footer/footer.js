cdae92c1763146ab4d146ad4d15cd3048fc1d0fd
Eddyking1 <edward.kumerius@hotmail.com>
2020-05-19 07:31:04 +0200

    formStyles

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -11,6 +11,7 @@ export default function Footer() {
                 <h1>Kolla igenom WebbDev Dealsen!</h1>
                 {Slider(footerImages)}
     </div> */}
+    <p> Certificate </p>
     <a href="https://www.testdome.com/cert/2c13697ff13e4fbcb542f1ca0e22bfc4" class="testdome-certificate-stamp silver"><span class="testdome-certificate-name">Edward kumerius</span><span class="testdome-certificate-test-name">JavaScript </span><span class="testdome-certificate-card-logo">TestDome<br />Certificate</span></a>
             <div className="fContact">
                 <p>
